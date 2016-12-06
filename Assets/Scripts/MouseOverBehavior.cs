9420d25c0effea89f3918e28ef78800aa5e8cc2e
Roldan Gammad <rgammad@uci.edu>
2016-12-06 06:17:47 -0800

    first

---
+++ Assets/Scripts/MouseOverBehavior.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TileMap))]
+public class MouseOverBehavior : MonoBehaviour {
+
+    TileMap _tileMap;
+
+    Vector3 currentTileCoord;
+
+    public Transform highlight;
+
+    void Start()
+    {
+        _tileMap = GetComponent<TileMap>();
+    }
+
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (GetComponent<Collider>().Raycast(ray, out hitInfo, Mathf.Infinity))
+        {
+            int x = Mathf.FloorToInt(hitInfo.point.x / _tileMap.tileSize);
+            int z = Mathf.FloorToInt(hitInfo.point.z / _tileMap.tileSize);
+
+
+            currentTileCoord.x = x;
+            currentTileCoord.z = z;
+
+            highlight.transform.position = currentTileCoord * _tileMap.tileSize;
+
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(highlight, new Vector3(x, 0, z), Quaternion.identity, transform);
+            }
+        }
+        else { }
+    }
+
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class TileMap : MonoBehaviour
+{
+
+    public int sizeX = 25;
+    public int sizeZ = 25;
+    public int sizeY = 5;
+
+    public float tileSize = 1.0f;
+
+
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+    MeshCollider meshCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        BuildMesh();
+    }
+
+   public  void BuildMesh()
+    {
+        int numTiles = sizeX * sizeZ;
+        int numTriangles = numTiles * 2;
+
+        int vertSizeX = sizeX + 1;
+        int vertSizeZ = sizeZ + 1;
+        int numVerts = vertSizeX * vertSizeZ;
+        //create mesh data
+        Vector3[] vertices = new Vector3[numVerts];
+        Vector3[] normals = new Vector3[numVerts];
+        Vector2[] uv = new Vector2[numVerts];
+
+        int[] triangles = new int[numTriangles * 3];
+
+        int x, z;
+        for (z = 0; z < vertSizeZ; z++)
+        {
+            for (x = 0; x < vertSizeX; x++)
+            {
+                vertices[z * vertSizeX + x] = new Vector3(x * tileSize, 0, z * tileSize);
+                normals[z * vertSizeX + x] = Vector3.up;
+                uv[z * vertSizeX + x] = new Vector2((float)x / vertSizeX, (float)z / vertSizeZ);
+            }
+
+        }
+        for (z = 0; z < sizeZ; z++)
+        {
+            for (x = 0; x < sizeX; x++)
+            {
+                int tileIndex = z * sizeX + x;
+                int triangleOffset = tileIndex * 6;
+                int tileOffset = z * vertSizeX + x;
+
+                triangles[triangleOffset + 0] = tileOffset + 0;
+                triangles[triangleOffset + 1] = tileOffset + vertSizeX + 0;
+                triangles[triangleOffset + 2] = tileOffset + vertSizeX + 1;
+
+                triangles[triangleOffset + 3] = tileOffset + 0;
+                triangles[triangleOffset + 4] = tileOffset + vertSizeX + 1;
+                triangles[triangleOffset + 5] = tileOffset + 1;
+
+            }
+        }
+
+
+        //create mesh & populate
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        //assign mesh to filter/renderer/collider
+
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+}
